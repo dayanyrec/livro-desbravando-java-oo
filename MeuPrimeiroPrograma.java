a47ccfe1e370c3f0adf979f60273478eafce7256
Dayany Espindola <dayanyrec@gmail.com>
2016-09-01 21:55:51 -0300

    Pass command line args

--- MeuPrimeiroPrograma.java
+++ MeuPrimeiroPrograma.java
@@ -1,7 +1,7 @@
 class MeuPrimeiroPrograma {
 
   public static void main(String[] args) {
-    System.out.println("O primeiro de muitos!");
+    System.out.println("O primeiro de muitos códigos escritos em " + args[0] + " pela " + args[1] + "!");
   }
 
 }
